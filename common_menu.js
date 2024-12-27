9edc0e9745e993e57c6f64a074e54c9939bc2ec9
ShinSSH <gnstmdtls456@naver.com>
2024-12-27 10:32:04 +0900

    완료

--- common_menu.js
+++ common_menu.js
@@ -7,10 +7,10 @@ class Menu{
     mtitle;url;tips;
 }
 menu0 = new Menu("심심풀이 Racing Game")
-menu0.url = "https://dmsgur.github.io/classification_model/race.html"
+menu0.url = "https://ShinSSH.github.io/classification_model/race.html"
 menu0.tips = "아주 기본적이고 간단한 심심풀이 땅콩 레이싱 고고~"
 menu1 = new Menu("CNN AND Crawling");
-menu1.url="https://dmsgur.github.io/classification_model/"
+menu1.url="https://ShinSSH.github.io/classification_model/"
 menu1.tips="네이버,구글 이미지 크롤링 및 컨볼루션 적용한 모델";
 menu2 = new Menu("test menu")
 menu2.url = "127.0.0.1"
